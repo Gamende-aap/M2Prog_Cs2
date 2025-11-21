1ffe6f0303e62a818580af84b3a25eef9647c0f1
Tygo <40080@ma-web.nl>
2025-11-21 12:34:45 +0100

    l

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,12 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        program program = new program();
+        Program program = new Program();
         program.run(); //program heet het programma
 
     }
     internal void run()
     {
-        Console.writeLine("Dit is nu de start van mijn programma");
+        Console.WriteLine("Dit is nu de start van mijn programma");
     }
 }

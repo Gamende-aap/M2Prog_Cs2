2a2b5363cf31f8aef0eeba44ec40caa16fbe2b58
Tygo <40080@ma-web.nl>
2025-11-21 12:34:23 +0100

    commit

---
+++ Functions/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Functions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        program program = new program();
+        program.run(); //program heet het programma
+
+    }
+    internal void run()
+    {
+        Console.writeLine("Dit is nu de start van mijn programma");
+    }
+}
--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace M2Prog_Cs2;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}

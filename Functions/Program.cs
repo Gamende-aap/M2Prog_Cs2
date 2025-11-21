3e9ce85c99dc620dd622bb82ae1055aa698c7459
Tygo <40080@ma-web.nl>
2025-11-21 13:07:01 +0100

    5

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -8,14 +8,50 @@ class Program
         program.run(); //program heet het programma
 
     }
+    
+    internal void AskRandomQuestion()
+    {
+        string vraag = Getrandomvraag();
+        Console.WriteLine(vraag);
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    
+        string[] vragen = new string[]
+        {
+            "What secret conspiracy would you like to actually start letting other people know?",
+            "If you can still remember, what are your funniest childhood memories?",
+            "What would you do if you won a million dollars?",
+            "What is the worst game you played but that you liked anyway?",
+            "Why do round pizzas come in square boxes?",
+            "Has someone caught you dancing in front of the mirror?",
+            "What would you call a male ladybug?"
+        };
+    
+    internal string Getvraag(int vraagIndex)
+    {
+        return vragen[vraagIndex];
+    }
+
+    internal string Getrandomvraag()
+    {
+        Random random = new Random();
+        int index = random.Next(vragen.Length);
+        return vragen[index];
+    }
+
+
     internal void run()
     {
         Console.WriteLine("Dit is nu de start van mijn programma");
-        Vraag1();
-        Vraag2();
-        Vraag3();
-        Vraag4();
-        Vraag5();
+        AskRandomQuestion();
+        AskRandomQuestion();
+        AskRandomQuestion();
+        AskRandomQuestion();
+        AskRandomQuestion();
+        AskRandomQuestion();
+
+        
         Console.WriteLine("Dit is het einde van mijn programma");
     }
 
@@ -53,4 +89,20 @@ class Program
         string antwoord5 = Console.ReadLine();
         Console.WriteLine(antwoord5);
     }
+
+    internal void Vraag6()
+    {
+        Console.WriteLine("Has someone caught you dancing in front of the mirror?");
+        string antwoord6 = Console.ReadLine();
+        Console.WriteLine(antwoord6);
+    }
+
+    internal string Vraag7()
+    {
+        Console.WriteLine("What would you call a male ladybug?");
+        string antwoord7 = Console.ReadLine();
+        return antwoord7;
+    }
+
+    
 }

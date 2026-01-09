a134c3e369b51b48ba9438c9c9a3474f3f98ab03
Tygo <40080@ma-web.nl>
2026-01-09 12:30:18 +0100

    class

---
+++ class/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Class
+{
+    class Room
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Description { get; set; }
+
+        public Room(int x, int y, string description)
+        {
+            X = x;
+            Y = y;
+            Description = description;
+        }
+
+        internal string GetRoomLocationKey()
+        {
+            return $"{X},{Y}";
+        }
+    }
+
+    class Program
+    {
+        static Dictionary<string, Room> rooms = new Dictionary<string, Room>();
+
+        static void Main(string[] args)
+        {
+            // Add start room
+            rooms["0,0"] = new Room(0, 0, "You are in the start room.");
+
+            // Add more rooms
+            rooms["1,0"] = new Room(1, 0, "You are in the east room.");
+            rooms["0,1"] = new Room(0, 1, "You are in the north room.");
+            rooms["-1,0"] = new Room(-1, 0, "You are in the west room.");
+
+            // Show rooms
+            ShowRoom(0, 0);
+            ShowRoom(1, 0);
+            ShowRoom(0, 1);
+            ShowRoom(-1, 0);
+
+            // Try non-existing room
+            ShowRoom(2, 2);
+        }
+
+        private static void ShowRoom(int x, int y)
+        {
+            string key = $"{x},{y}";
+            if (rooms.ContainsKey(key))
+            {
+                Room room = rooms[key];
+                Console.WriteLine(room.Description);
+            }
+            else
+            {
+                Console.WriteLine("The room does not exist.");
+            }
+        }
+    }
+}

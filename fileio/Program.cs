a320611b9f168a773a1a20ca679e3403e41b6a54
Tygo <40080@ma-web.nl>
2026-01-16 11:51:16 +0100

    FileIo 0.1

--- fileio/Program.cs
+++ fileio/Program.cs
@@ -8,10 +8,21 @@ class Program
     static void Main(string[] args)
     {
         Run();
+        string[] lines = File.ReadAllLines("quiz.txt");
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+        string[] lines2 = File.ReadAllLines("nos.txt");
+        foreach (string line in lines2)
+        {
+            Console.WriteLine(line);
+        }
     }
 
     static void Run()
     {
         Console.WriteLine("Project is running!");
+        Console.WriteLine(Environment.CurrentDirectory);
     }
 }

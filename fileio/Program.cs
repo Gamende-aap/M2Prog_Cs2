08ccda198d5a4f89157b2594e8d009f95297bce7
Tygo <40080@ma-web.nl>
2026-01-16 11:46:20 +0100

    FileIo 0.0

---
+++ fileio/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace fileio;
+
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Run();
+    }
+
+    static void Run()
+    {
+        Console.WriteLine("Project is running!");
+    }
+}

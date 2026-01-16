a8ef7bf5233fc00e60b4299fb05168e1c75efcb5
Tygo <40080@ma-web.nl>
2026-01-16 12:11:31 +0100

    fileio 0.2

---
+++ Fileio,/fileio 0.2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace fileio_0._2;
+
+using System;
+
+using System.IO;
+ 
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        // Bestandsnaam waar we de tekst bewaren
+
+        string saveFile = "welkomState.txt";
+ 
+        // 1. Lezen van de vorige opgeslagen tekst (als die bestaat)
+
+        if (File.Exists(saveFile))
+
+        {
+
+            string oudeTekst = File.ReadAllText(saveFile);
+
+            Console.WriteLine("Vorige tekst: " + oudeTekst);
+
+        }
+
+        else
+
+        {
+
+            Console.WriteLine("Geen bestaande tekst gevonden.");
+
+        }
+ 
+        // 2. Nieuwe tekst vragen
+
+        Console.WriteLine("Typ een nieuwe tekst:");
+
+        string nieuweTekst = Console.ReadLine();
+ 
+        // 3. Opslaan naar het bestand
+
+        File.WriteAllText(saveFile, nieuweTekst);
+ 
+        Console.WriteLine("Je tekst is opgeslagen!");
+
+    }
+
+}
+
+ 
